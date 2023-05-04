5713010f7aac0208d8379a03987dce9f44ab4da7
Florent Shabani <108886409+FlorentShabani@users.noreply.github.com>
2023-05-04 17:35:31 +0200

    Converted to ASP .NET

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travista.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            ViewData["Title"] = "Manage Users";
+            return View("~/Views/Users/Index.cshtml");
+        }
+    }
+}
